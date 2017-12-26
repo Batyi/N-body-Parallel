42c9be61777ec53b35295c86a3dfe8b1b14b63f4
batyi <pascal.german.91@mail.ru>
2017-12-27 01:52:00 +0600

    Parallelization of SimulateBruteforce algorithm

--- Readme.md
+++ Readme.md
@@ -1,1 +1,12 @@
 N-body Parallel project with MPI
+Parallelization of SimulateWithBruteforce algorithm
+
+By default:
+item_per_process = 10 (number of planets in each process)
+numberOfPlanets = 100
+simulationTime = 1.0
+You can change in main function;
+
+Compilation:
+mpicc project.c -o project -lm  (write -lm because we use math functions like (cos, sin))
+mpiexec -n 10 ./project
--- project.c
+++ project.c
@@ -13,21 +13,15 @@ typedef struct _body {
         float vx, vy; // velocity
         float mass; // mass
 } body;
-static bool IsItFirst = true;
 static body *planets;
-static body *planetsTemp;
-static int items_per_process;
-static int numberOfPlanets;
 static const float DT = 0.1;
 static double parallel_average_time = 0.0;
 static double serial_average_time = 0.0;
-void GenerateDebudData();
-void SimulateWithBruteforce(int *argc, char ***argv);
 void CalculateNewtonGravityAcceleration(body *a, body *b, float *ax, float *ay) {
         float galacticPlaneRofX = b->x - a->x;
         float galacticPlaneRofY = b->y - a->y;
         float simulationSofteningLengthSquared = 10;
-        float distanceSquared = (a->x * a->x + a->y * a->y) + simulationSofteningLengthSquared;
+        float distanceSquared = sqrt(a->x * a->x + a->y * a->y) + simulationSofteningLengthSquared;
         float distanceSquaredCubed = distanceSquared * distanceSquared * distanceSquared;
         float inverse = 1.0 / sqrt(distanceSquaredCubed);
         float scale = b->mass * inverse;
@@ -40,91 +34,109 @@ void integrate(body *planet, float deltaTime) {
         planet->vx += planet->ax * DT;
         planet->vy += planet->ay * DT;
 }
-void GenerateDebugData () {
+void GenerateDebugData (int numberOfPlanets) {
         const float accelerationScale = 100.0;
         srand(time(NULL));
         //printf("%d\n", rand());
         for (int i = 0; i < numberOfPlanets; i++) {
-                float angle = ((float) i / numberOfPlanets) * 2.0 * PI + ((rand() % 1000 - 0.5) * 0.5);
-                float initialMass = 5.0;
-                body planet = {.x = rand() % 1000, .y = rand() % 1000, .vx = cos(angle) * accelerationScale * (rand() % 1000), .vy = sin(angle) * accelerationScale * (rand() % 1000), .mass = initialMass * (rand() % 1000) + initialMass * 0.5};
+                float angle = ((float) i / numberOfPlanets) * 2.0 * PI + (((rand() % 10) / 10.0 - 0.5) * 0.5);
+                float initialMass = 1.0;
+                body planet = {.x = (rand() % 10) / 10.0, .y = (rand() % 10) / 10.0, .vx = cos(angle) * accelerationScale * ((rand() % 10) / 10.0), .vy = sin(angle) * accelerationScale * ((rand() % 10) / 10.0), .ax = 0.0, .ay = 0.0, .mass = initialMass * ((rand() % 10) / 10.0) + initialMass * 0.5};
                 float scale = (planet.mass / (initialMass * 1.5)) + 0.1;
                 planets[i] = planet;
         }
 }
-void SimulateWithBruteforce(int *argc, char ***argv) {
-        MPI_Init(argc, argv);
+void SimulateWithBruteforce(int *argc, char ***argv, int items_per_process, int numberOfPlanets, float DT, float simulationTime) {
+		MPI_Init(argc, argv);
         int world_size;
         MPI_Comm_size(MPI_COMM_WORLD, &world_size);
         int rank;
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-        printf("Process %d out of %d\n", rank, world_size);
         if(rank == 0) {
-                parallel_average_time -= MPI_Wtime();
-                numberOfPlanets = world_size * items_per_process;
-                printf("%d, %d, %d\n", numberOfPlanets, world_size, items_per_process);
-                planets = (body*) malloc(numberOfPlanets * sizeof(*planets));
-                planetsTemp = (body*) malloc(numberOfPlanets * sizeof(*planets));
-                if(IsItFirst == true) {
-                        IsItFirst = false;
-                        GenerateDebugData();
-                        planetsTemp = planets;
-                }
+            parallel_average_time -= MPI_Wtime();
+			printf("numberOfPlanets = %d\n", numberOfPlanets);
+			printf("DT = %f\n", DT);
+			for(int j = 0; j < numberOfPlanets; j++) {
+				printf("planets #%d position of X = %f, of Y = %f\n", j + 1, planets[j].x,  planets[j].y);
+				printf("planets #%d acceleration of X = %f, of Y = %f\n", j + 1, planets[j].ax,  planets[j].ay);
+				printf("planets #%d velocity of X = %f, of Y = %f\n", j + 1, planets[j].vx,  planets[j].vy);
+				printf("planets #%d mass = %f\n", j + 1, planets[j].mass);
+			}
         }
-        MPI_Barrier(MPI_COMM_WORLD);
         MPI_Datatype planetType;
         MPI_Type_contiguous(7, MPI_FLOAT, &planetType);
         MPI_Type_commit(&planetType);
         body *local_numbers = (body *) malloc(sizeof(*local_numbers) * items_per_process);
-        MPI_Barrier(MPI_COMM_WORLD);
         MPI_Scatter(planets, items_per_process, planetType, local_numbers, items_per_process, planetType, 0, MPI_COMM_WORLD);
-        printf("                Process %d out of %d\n", rank, world_size);
-//      printf("AAAAAAAAAA                                                      %f\n", planets[0].x);
-        for(int i = 0; i < items_per_process; i++) {
-                float total_ax = 0, total_ay = 0;
-                for (size_t j = 0; j < numberOfPlanets; j++) {
-                        if (i == j) {continue;}
-                        float ax = 0, ay = 0;
-                        CalculateNewtonGravityAcceleration(&local_numbers[i], &planetsTemp[j], &ax, &ay);
-                        printf("%f, %f\n", ax, ay);
-                        total_ax += ax;
-                        total_ay += ay;
-                }
-                local_numbers[i].ax = total_ax;
-                local_numbers[i].ay = total_ay;
-        }
-        printf("Local number %f\n", local_numbers[0].ax);
-        //printf("        Process %d out of %d\n", rank, world_size);
-        MPI_Gather(local_numbers, items_per_process, planetType, planets, items_per_process, planetType, 0, MPI_COMM_WORLD);
-        //printf("             Process %d out of %d\n", rank, world_size);
+        
+		
+		for(double time = 0; time < 1; time += DT) {
+			for(int i = 0; i < items_per_process; i++) {
+					float total_ax = 0.0, total_ay = 0.0;
+					for (size_t j = 0; j < numberOfPlanets; j++) {
+							if (i + rank * items_per_process == j) {continue;}
+							float ax = 0, ay = 0;
+							CalculateNewtonGravityAcceleration(&local_numbers[i], &planets[j], &ax, &ay);
+							total_ax += ax;
+							total_ay += ay;
+					}
+					local_numbers[i].ax = total_ax;
+					local_numbers[i].ay = total_ay;
+					printf("Time = %f, planets #%d acceleration of X = %f, of Y = %f\n", time, i + rank * items_per_process + 1, local_numbers[i].ax, local_numbers[i].ay);
+					integrate(&local_numbers[i], DT);
+			}
+		}
+		
+		body *planetsTemp = (body*) malloc(numberOfPlanets * sizeof(*planetsTemp));
+        MPI_Gather(local_numbers, items_per_process, planetType, planetsTemp, items_per_process, planetType, 0, MPI_COMM_WORLD);
         if(rank == 0) {
                 parallel_average_time += MPI_Wtime();
-                printf("numberOfPlanets %d, DT %f\n", numberOfPlanets, DT);
                 printf("Parallel Process Time: %f\n", parallel_average_time);
-                for(int i = 0; i < numberOfPlanets; i++) {
-                        integrate(&planets[i], DT);
-                        printf("planets #%d acceleration of X = %f, of Y = %f\n", i + 1, planets[i].ax,  planets[i].ay);
-                }
         }
+		if(planets != NULL) {
+			free(planets);
+		}
         free(local_numbers);
+		if(planetsTemp != NULL) {
+			free(planetsTemp);
+		}
         MPI_Finalize();
 }
 
 int main(int argc, char **argv) {
         parallel_average_time = 0.0;
-        serial_average_time = 0.0;
-        items_per_process = 10;
-
-        //numberOfPlanets = world_size * items_per_process;
-        //planets = (body*) malloc(numberOfPlanets * sizeof(*planets));
-        //GenerateDebugData(numberOfPlanets);
-        //printf("numberOfPlanets %d, DT %f\n", numberOfPlanets, DT);
-        //for(double i = 0; i < 1; i += DT) {
-                SimulateWithBruteforce(&argc, &argv);
-        //      for(int j = 0; j < numberOfPlanets; j++) {
-        //              printf("planets #%d acceleration of X = %f, of Y = %f\n", j + 1, planets[j].ax,  planets[j].ay);
-        //      }
-        //}
-
+        //serial_average_time = 0.0;
+        int items_per_process = 10;
+		int numberOfPlanets = 100;
+		float simulationTime = 1.0;
+		planets = (body*) malloc(numberOfPlanets * sizeof(*planets));
+		GenerateDebugData (numberOfPlanets);
+        SimulateWithBruteforce(&argc, &argv, items_per_process, numberOfPlanets, DT, simulationTime);
         return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
